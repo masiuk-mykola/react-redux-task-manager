3d7591e6ec28075e33d70df5e8bd849a5ba41694
Mykola Masiuk <masiuk.mykola@gmail.com>
2022-09-19 15:46:21 +0300

    fix bug

--- src/redux/reducer.js
+++ src/redux/reducer.js
@@ -11,11 +11,11 @@ const tasksInitialState = [
 
 const tasksReducer = (state = tasksInitialState, action) => {
   switch (action.type) {
-    case 'tasks/addTask':
+    case 'task/addTask':
       return [...state, action.payload];
-    case 'tasks/deleteTask':
+    case 'task/deleteTask':
       return state.filter(task => task.id !== action.payload);
-    case 'tasks/toggleCompleted':
+    case 'task/toggleCompleted':
       return state.map(task => {
         if (task.id !== action.payload) {
           return task;
--- src/redux/store.js
+++ src/redux/store.js
@@ -3,5 +3,4 @@ import { devToolsEnhancer } from '@redux-devtools/extension';
 import { rootReducer } from './reducer';
 
 const enhancer = devToolsEnhancer();
-
 export const store = createStore(rootReducer, enhancer);
